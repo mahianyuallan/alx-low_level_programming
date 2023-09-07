3d2308cf70247c77997c0fb68f966fcf4cb8386a
mahianyuallan <mwenjaallan18@gmail.com>
2023-09-06 23:42:35 -0700

    Hello prinf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main - Entry point
  * Description: 'Program that uses printf to print'
  * Return: 0 and exit the program
- * */
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
