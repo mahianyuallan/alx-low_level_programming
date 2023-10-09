b4c834e5a5b50cd24e34acb77704c7949e3c5962
mahianyuallan <mwenjaallan18@gmail.com>
2023-10-08 22:21:31 -0700

    Task completed

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Free dogs.
+ * @d: type pointer struct data.
+ * Return: 0.
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
