9895c8d78e3e3ac30b80bd7a36eb12dd5cee82e6
Allan Mwenja <140613367+mahianyuallan@users.noreply.github.com>
2023-10-25 13:24:08 +0300

    Completed ✔️

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_isupper - a function that checks for uppercase characters
+ *_isupper - a function that checks for all uppercase characters
  *@c: a variable that stores letters
  *Return: Returns a value of 1 if the variable c contains an uppercase
  *letter.
